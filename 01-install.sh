1c0b43e723db8a39bb7fcfdf4d81035ed4cdc9ce
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-08-31 08:17:57 +0530

    shell script started

--- 01-install.sh
+++ 01-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-AMI_ID=0220d79f3f480ecf5
-ZONE_ID=Z083970011QGFE38SJQH9
+AMI_ID="0220d79f3f480ecf5"
+ZONE_ID="Z083970011QGFE38SJQH9"
 DOMAIN_NAME=mohammed.world
 
 for instance in $@
@@ -14,9 +14,8 @@ do
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
-    )
-
-    echo "instance_ID:$INSTANCE_ID"
+        )
+    echo "INSTANCE_ID=$INSTANCE"
     if [ $instance == frontend ]; then
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
@@ -26,16 +25,14 @@ do
     else
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
-        --query "Reservations[*].Instances[*].PrivateIpAddress" \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
         --output text)
     R53_RECORD=$instance.$DOMAIN_NAME
     fi
-
-### updating R53 Records ###
- aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-            {
+    ### Updating R53_RECORD ###
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id "$ZONE_ID" \
+        --change-batch   '          {
                 "Comment": "Update A record to new IP",
                 "Changes": [
                     {
