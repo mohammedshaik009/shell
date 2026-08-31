ab304de733e768b4ab6da7586f29231bdfcd1a0c
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-08-31 10:26:03 +0530

    shell script started

--- 02-install.sh
+++ 02-install.sh
@@ -14,7 +14,7 @@ if [ $# -lt 2 ]; then
 fi
 
 ACTION=$1
-shift #first argument willbe removes
+shift #first argument will be removes
 
 if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete"]; then
     echo -e "$R ERROR:: First argument must be either create or delete $N"
@@ -34,7 +34,7 @@ do
         if [ $INSTANCE_ID == "None" ]; then
         echo "Launching instance: roboshop-$instance"
         INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id ami-0220d79f3f480ecf5 \
+        --image-id $AMI_ID \
         --instance-type t3.micro \
         --security-groups "roboshop-common" "roboshop-$instance" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
@@ -42,9 +42,48 @@ do
         --output text
         )
         echo "Launched instance: $INSTANCE_ID"
+
+        #Update R53_RECORD
+    if [ $instance == frontend ]; then
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
+        --output text
+        )
+        R53_RECORD=$DOMAIN_NAME
     else
-        echo "roboshop $instance is already running: $INSTANCE_ID"
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PrivateIpAddress" \
+        --output text
+        )
+        R53_RECORD=$instance.$DOMAIN_NAME
+    fi
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+            {
+                "Comment": "Update A record to new IP",
+                "Changes": [
+                    {
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": "'$R53_RECORD'",
+                            "Type": "A",
+                            "TTL": 1,
+                            "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    '
+    echo "updated R53_RECORD for :$instance"
+ else
+      echo "roboshop $instance is already running: $INSTANCE_ID"
         fi
     fi
-
-done
+   
