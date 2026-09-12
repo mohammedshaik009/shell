dd957bd0d6af6dd15dc2fe08c7980a8557899c8b
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-12 20:04:30 +0530

    shell script started

--- 05-logs-cleanup.sh
+++ 05-logs-cleanup.sh
@@ -8,4 +8,8 @@ if [ -z $SOURCE_DIR ]; then
     echo "USAGE:: $0 <source_dir> [days (optional to 14)]"
     exit 1
 fi
- 
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "ERROR:: directory does not exist"
+    exit 1
+fi
