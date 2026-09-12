6eb7ab8839c754675ea8bfa0b95ba357e7ac3c7f
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-12 20:30:48 +0530

    shell script started

--- 05-logs-cleanup.sh
+++ 05-logs-cleanup.sh
@@ -17,3 +17,7 @@ fi
 echo "scanning $SOURCE_DIR for log files older than 14 days"
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
+if [ -z $FILES ]; then
+    echo "no log files older than 14 days found"
+    exit 0
+fi
