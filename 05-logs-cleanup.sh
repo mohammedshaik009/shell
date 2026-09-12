5201c4f9536e27bc4236244366bf0f700707b505
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-12 17:42:58 +0530

    shell script started

---
+++ 05-logs-cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DAYS=${2:-14} #default to 14 days
+
+if [ -z $SOURCE_DIR ]; then
+    echo "ERROR::Missing parameters"
+    echo "USAGE:: $0 <source_dir> [days(optional to 14)]"
+    exit 1
+fi
