7741d302bb55def8e2b754d120d1f331ccdff78d
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-12 20:02:08 +0530

    shell script started

--- 05-logs-cleanup.sh
+++ 05-logs-cleanup.sh
@@ -4,27 +4,8 @@ SOURCE_DIR=$1
 DAYS=${2:-14} #default to 14 days
 
 if [ -z $SOURCE_DIR ]; then
-    echo "ERROR::Missing parameters"
-    echo "USAGE:: $0 <source_dir> [days(optional to 14)]"
+    echo "ERROR:: Missing parameters"
+    echo "USAGE:: $0 <source_dir> [days (optional to 14)]"
     exit 1
 fi
-
-if [ ! -d $SOURCE_DIR ]; then
-    echo "ERROR::directory is :$SOURCE_DIR does not exist"
-    exit 1
-fi
-
-echo "scanning $SOURCE_DIR for log files older than 14 days"
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
-
-if [ -z "$FILES" ]; then
-    echo "no log files older than 14 days found"
-    exit 0
-fi
-
-while IFS= read -r FILE
-do 
-    echo "file to be deleted: $FILE"
-    rm -f $FILE
-    echo "file $FILE deleted"
-done <<< "$FILES"
+ 
