ac1c05b00431bb3bd81779c929392040853aea94
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-12 21:31:06 +0530

    shell script started

--- 05-logs-cleanup.sh
+++ 05-logs-cleanup.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
 SOURCE_DIR=$1
-DAYS=${2:-14} #default to 14 days
 
-if [ -z $SOURCE_DIR ]; then
+DAYS=${2:-14} # default to 14 days
+
+if [ -z "$SOURCE_DIR" ]; then
+
     echo "ERROR::Missing parameters"
+
     echo "USAGE: $0 <source_dir> [days(optional to 14)]"
+
     exit 1
+
 fi
 
-if [ ! -d $SOURCE_DIR ]; then
-    echo "ERROR::directory does not exist"
+if [ ! -d "$SOURCE_DIR" ]; then
+
+    echo "ERROR::directory $SOURCE_DIR does not exist"
+
     exit 1
+
 fi
 
-echo "scanning $SOURCE_DIR for no log files older 14 days"
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+echo "Scanning $SOURCE_DIR for log files older than $DAYS days"
 
-if [ -z $FILES ]; then
-    echo "no log files older 14 days found"
+FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +"$DAYS")
+
+if [ -z "$FILES" ]; then
+
+    echo "No log files older than $DAYS days found"
+
     exit 0
+
 fi
 
 while IFS= read -r FILE
 do
-    echo "Files to deleted: $FILE"
-    rm -f $FILE
-    echo "Files $FILE deleted"
-done <<< $FILES
+
+    echo "File to be deleted: $FILE"
+
+    rm -f "$FILE"
+
+    echo "File $FILE deleted"
+
+done <<< "$FILES"
