bb43b736b98ed7d7cbe13af516908900dd41723a
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-13 17:27:29 +0530

    shell script started

--- 06-archieve.sh
+++ 06-archieve.sh
@@ -4,18 +4,18 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #default to 14 days
 
-if [ -z $SOURCE_DIR ] || [ -z $DEST_DIR ]; then
+if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
     echo "ERROR::Either source directory  or destination dir empty"
     echo "USAGE:: $0 <source_dir> <dest_dir> [days:default to 14]"
     exit 1
 fi
 
-if [ ! -d $SOURCE_DIR ]; then
+if [ ! -d "$SOURCE_DIR" ]; then
     echo "source directory: $SOURCE_DIR does not exist"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR ]; then
+if [ ! -d "$DEST_DIR" ]; then
     echo "dest_directory: $DEST_DIR does not exist"
     exit 1
 fi
