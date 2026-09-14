a47705db652bbb6f320412f5fba4c3f259dd8ee0
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-14 11:17:02 +0530

    shell script started

--- 07-logs-archieve.sh
+++ 07-logs-archieve.sh
@@ -4,24 +4,24 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #default to 14 days
 
-if [ -z "$SOURCE_DIR" ]; then
+if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
     echo "ERROR:: Either source_dir or Dest_dir Missing"
     echo "USAGE:: $0 <source_dir> <dest_dir> (days[default to 14])"
     exit 1
 fi
 
 if [ ! -d "$SOURCE_DIR" ]; then
-    echo "directory "$SOURCE_DIR" does not exist"
+    echo "directory $SOURCE_DIR does not exist"
     exit 1
 fi
 
 if [ ! -d "$DEST_DIR" ]; then
-    echo "directory "$DEST_DIR" does not exist"
+    echo "directory $DEST_DIR does not exist"
     exit 1
 fi
 
-echo "scanning for $SOURCE_DIR $DEST_DIR for log files older than 14 days"
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+echo "scanning for $SOURCE_DIR log files older than $DAYS"
+FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +$DAYS)
 
 if [ -z "$FILES" ]; then
     echo "no log files found older than 14 days"
@@ -29,7 +29,7 @@ if [ -z "$FILES" ]; then
 fi
 
 TIMESTAMP=$(date "+%Y-%m-%d %H-%M-%S")
-ARCHIEVE_FILE="$DEST_DIR/logs-archieve $TIMESTAMP tar.gz.log"
+ARCHIEVE_FILE="$DEST_DIR/logs-archieve-$TIMESTAMP tar.gz"
 
 tar -czvf "$ARCHIEVE_FILE" $FILES
 
@@ -40,7 +40,7 @@ if [ $? -eq 0 ]; then
             echo "File is $FILE"
             rm -f "$FILE"
             echo "Delete File $FILE"
-    done <<< $FILES
+    done <<< "$FILES"
 else
     echo "ERROR:archieval is FAILED"
     exit 1
