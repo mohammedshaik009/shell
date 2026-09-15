f8b8125f68e82a3480d18ba89023b13b4ce2e8bd
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-15 10:15:27 +0530

    shell script started

--- 06-logs-archeive.sh
+++ 06-logs-archeive.sh
@@ -2,42 +2,45 @@
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14} #deafult to 14 days
+DAYS=${3:-14}
 
 if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
-    echo "ERROR:: Either source_dir or dest_dir is Missing"
-    echo "USAGE:: $0 <source_dir> <dest_dir> [14(optional default to days)]"
+    echo "Either source directory or destination directory empty"
+    echo "USAGE:: $0 [source_dir] [dest_dir] [days:default 14]"
     exit 1
 fi
 
 if [ ! -d "$SOURCE_DIR" ]; then
-    echo "directory $SOURCE_DIR does not exist"
+    echo "Source directory: $SOURCE_DIR does not exist"
     exit 1
 fi
 
 if [ ! -d "$DEST_DIR" ]; then
-    echo "directory $DEST_DIR does not exist"
+    echo "Destination directory: $DEST_DIR does not exist"
     exit 1
 fi
 
-echo "scanning for $SOURCE_DIR $DEST_DIR log files older than 14 days"
-FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +$DAYS)
+FILES=$( find "$SOURCE_DIR" -type f -name "*.log" -mtime +$DAYS )
 
 if [ -z "$FILES" ]; then
-    echo "no log files found older than 14 days"
+    echo "Log files older than 14 days not found, nothing to do"
     exit 0
 fi
 
-TIMESTAMP=$(date "+%Y-%m-%s-%H-%M-%S")
-ARCHEIVE_FILE="$DEST_FILE/logs-archeive-$TIMESTAMP.tar.gz"
 
-tar -czvf "$ARCHEIVE_FILE" $DEST_DIR
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+ARCHIEVE_FILE="$DEST_DIR/logs-archieve-$TIMESTAMP.tar.gz"
+
+tar -czvf $ARCHIEVE_FILE $FILES
 
 if [ $? -eq 0 ]; then
-    echo "archeival is success, deleting the files"
-        while IFS= read -r FILE
-        do
-            echo "file is $FILE"
-            rm -f "$FILE"
-        echo "file $FILE deleted"
-done <<< "$FILES"
+    echo "Archieval is success, deleting the files"
+    while IFS= read -r FILE
+    do
+        rm -f $FILE
+        echo "Deleted file: $FILE"
+    done <<< "$FILES"
+else
+    echo "ERROR:: Archieval is failed"
+    exit 1
+fi
